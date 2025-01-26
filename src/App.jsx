c1dfb1b11d4e9727cc7efe17128202215bbfe492
A-Mikayelyan <a.mikayelyyan@gmail.com>
2025-01-26 17:58:58 +0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ToDoList from './ToDoList';
+
+function App(){
+
+  
+
+
+  return(
+    <>
+
+    <ToDoList/>
+
+    
+       
+    
+    </>
+  )
+
+}
+
+
+
+
+
+  
+
+  
+
+  
+
+
+export default App 
